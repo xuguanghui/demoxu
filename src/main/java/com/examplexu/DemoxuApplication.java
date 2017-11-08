d94daa1258efed9afe17bac3f24c29da4bc5dcea
xuguanghui <1940766849@qq.com>
2017-11-08 18:52:40 +0800

    Initial commit

---
+++ src/main/java/com/examplexu/DemoxuApplication.java
@@ -0,0 +1,13 @@
+package com.examplexu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoxuApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoxuApplication.class, args);
+	}
+}
---
+++ src/main/java/com/examplexu/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.examplexu;
+
+/**
+ * Created by Administrator on 2017/11/5.
+ */
+public class HelloWorld {
+    public  static  void  main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
